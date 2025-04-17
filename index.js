227f55444a51f6624f6d10bd121cc16de6d260cb
fedorka3 <35789@ocvts.org>
2025-04-17 11:51:38 +0000

    x shows up js navbar

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('hamburger-menu')
+const x = document.getElementById('hamburger-x')
 const navLinks = document.querySelector('.nav-links');
+
+open = false
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('mobile');
+
+  // Switches to the x i instead of the menu i
+  if (open == false) {
+    open = true;
+    menu.style.display = 'none';
+    x.style.display = 'block';
+  } else {
+    open = false;
+    menu.style.display = 'block';
+    x.style.display = 'none';
+  }
+
 });
